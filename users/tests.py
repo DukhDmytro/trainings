da844fc8030073e8927ab205814e74bd7232cbc5
DukhDmytro <cowboybebop4991@gmail.com>
2020-03-08 15:35:39 +0200

    Tests for users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        # registration of user
+        data = {'username': 'test_user', 'email': 'test@gmail.com', 'password': '12345'}
+        response = self.client.post('http://127.0.0.1:8000/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # registration of existing user
+        data = {'username': 'test_user', 'email': 'test@gmail.com', 'password': '12345'}
+        response = self.client.post('http://127.0.0.1:8000/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login(self):
+        # registration of user
+        data = {'username': 'test_user', 'email': 'test@gmail.com', 'password': '12345'}
+        response = self.client.post('http://127.0.0.1:8000/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # get token
+        data = {'username': 'test_user', 'email': 'test@gmail.com', 'password': '12345'}
+        response = self.client.post('http://127.0.0.1:8000/login/', data)
+        self.assertTrue('access' in response.data and 'refresh' in response.data)
+        # get token with invalid username
+        data = {'username': 't', 'email': 'test@gmail.com', 'password': '12345'}
+        response = self.client.post('http://127.0.0.1:8000/login/', data)
+        self.assertFalse('access' in response.data and 'refresh' in response.data)
+        # get token with invalid password
+        data = {'username': 'test_user', 'email': 'test@gmail.com', 'password': '125'}
+        response = self.client.post('http://127.0.0.1:8000/login/', data)
+        self.assertFalse('access' in response.data and 'refresh' in response.data)
